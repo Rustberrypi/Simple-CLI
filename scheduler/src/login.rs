40a39f131d68f44a85ee2733e9f4595cf90f14eb
snubeagle <rmccull7@msudenver.edu>
2020-11-21 15:00:07 -0700

    added second user functionality to login module

--- scheduler/src/login.rs
+++ scheduler/src/login.rs
@@ -1,6 +1,7 @@
 #![allow(unused_imports, dead_code, unused_must_use, unused, unknown_lints, non_camel_case_types, non_snake_case)]
 use std::io::{self, Read};
 use std::fs::File;
+//extern crate rpasswd;
 
 fn main() {
     let mut loops = 2;
@@ -18,7 +19,8 @@ fn main() {
     
     f.read_to_string(&mut raw_creds);
     let mut data = raw_creds.lines();
-    let gCreds = {cred_struct { uname: String::from(data.next().unwrap()), passd: String::from(data.next().unwrap()) }};
+    let gCreds  = {cred_struct { uname: String::from(data.next().unwrap()), passd: String::from(data.next().unwrap()) }};
+    let okCreds = {cred_struct { uname: String::from(data.next().unwrap()), passd: String::from(data.next().unwrap()) }}; 
 
     while loops < 3 {
         println!("Enter username:");
@@ -27,6 +29,8 @@ fn main() {
         io::stdin().read_line(&mut upassd);
         if uname == gCreds.uname && upassd == gCreds.passd {
            loops = 3;
+        } else if uname == okCreds.uname && upassd == okCreds.passd {
+            loops = 3;
         } else {
            loops = loops + 1;
         }
