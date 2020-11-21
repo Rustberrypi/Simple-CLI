443fa9257a0ac4df5f886fce84ba2efd3a2ad607
snubeagle <rmccull7@msudenver.edu>
2020-11-21 14:06:37 -0700

    adding logic to check user input pw with whats saved

--- scheduler/src/login.rs
+++ scheduler/src/login.rs
@@ -1,4 +1,4 @@
-#![allow(unused_imports, dead_code, unused_must_use, unused, unknown_lints)]
+#![allow(unused_imports, dead_code, unused_must_use, unused, unknown_lints, non_camel_case_types, non_snake_case)]
 use std::io::{self, Read};
 use std::fs::File;
 
@@ -6,30 +6,29 @@ fn main() {
     let mut loops = 2;
     let mut uname = String::new();
     let mut upassd = String::new();
-    let file_creds = "./nothing.key";
+    let file_creds = "./.nothing.key";
     let mut raw_creds = String::new();
 
     struct cred_struct {
        uname: String, 
-        passd: String
+       passd: String
     };
 
     let mut f = File::open(file_creds).expect("Unable to read credentials file.");
     
     f.read_to_string(&mut raw_creds);
     let mut data = raw_creds.lines();
-    let gCreds = {cred_struct { uname: String::from(data.next().unwrap()), passd: String::from(data.next().unwrap())}};
+    let gCreds = {cred_struct { uname: String::from(data.next().unwrap()), passd: String::from(data.next().unwrap()) }};
 
-    println!("{}",gCreds.passd);
-
-//    while loops < 3 {
-//        println!("Enter username:\n");
-//        io::stdin().read_line(&mut uname);
-//        println!("Enter password:\n");
-//        io::stdin().read_line(&mut upassd);
-//        println!("Username input: {}", uname);
-//        println!("Password input: {}", upassd);
-
-//        loops = loops + 1;
-//    }
+    while loops < 3 {
+        println!("Enter username:");
+        io::stdin().read_line(&mut uname);
+        println!("Enter password:");
+        io::stdin().read_line(&mut upassd);
+        if uname == gCreds.uname && upassd == gCreds.passd {
+           loops = 3;
+        } else {
+           loops = loops + 1;
+        }
+    }
 }
