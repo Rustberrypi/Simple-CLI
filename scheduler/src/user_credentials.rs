b2c47db76f27eac141e7a1ed176e030af84e9cdc
Nate Roberts <robernat@msudenver.edu>
2020-12-04 21:55:05 -0700

    User credentials module still rough, but should be functional

--- scheduler/src/user_credentials.rs
+++ scheduler/src/user_credentials.rs
@@ -1,19 +1,66 @@
 extern crate argon2;
 
+use std::boxed::Box;
 use std::error::Error;
+use std::fmt;
 use std::fs::File;
-use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::io::{self, BufReader, Read, Write};
 use rand::RngCore;
 use rand::rngs::OsRng;
 
 const KEY_FILE: &str = "./.nothing.key";
-const MAX_ENTRY_LENGTH: usize = 64;
+const LARGE_BLOCK_SIZE: usize = 64;
+const SMALL_BLOCK_SIZE: usize=16;
 
 pub struct UserCred {
-	name: [u8; MAX_ENTRY_LENGTH],
+	name: Vec<u8>,
 	access: u8,
-	salt: [u8; 16],
-	pash: [u8; MAX_ENTRY_LENGTH],
+	salt: Vec<u8>,
+	pash: Vec<u8>,
+}
+
+// TODO: Create functions to translate this enum to & from single bytes
+pub enum AccessLevel {
+	ReadOnly,
+	ReadWrite,
+}
+
+impl fmt::Display for UserCred {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		write!(f, "{}{}{}{}",
+				String::from_utf8_lossy(&self.name),
+				self.access.to_string(),
+				String::from_utf8_lossy(&self.salt),
+				String::from_utf8_lossy(&self.pash),
+			)
+	}
+}
+
+fn turn_key() -> Result<Vec<UserCred>, &'static str> {
+	const ARGON: &str = "$argon2i$v=19$m=4096,t=3,p=1"; // 28 bytes
+	let key = File::open(KEY_FILE).expect("Unable to open key file!");
+	let mut reader = BufReader::new(key);
+	let mut contents: Vec<u8> = vec![];
+	reader.read(&mut contents).expect("Unexpected failure reading key file.");
+	let keylib = load_creds(contents);
+	Ok(keylib)
+}
+
+fn load_creds(inpt: Vec<u8>) -> Vec<UserCred> {
+	let mut users: Vec<UserCred> = vec!();
+	let mut i: usize = 0;		
+	while i < inpt.len() {
+		// TODO: Find a way to not use magic numbers
+		let user: UserCred = UserCred {
+			name: inpt[0..64].to_vec(),
+			access: inpt[64],
+			salt: inpt[65..81].to_vec(),
+			pash: inpt[81..145].to_vec(),
+		};
+		users.push(user);
+		i += 145;
+	}
+	users
 }
 
 impl UserCred {
@@ -27,13 +74,13 @@ impl UserCred {
 		let mut tmp1 = String::new();
 		let mut tmp2 = String::new();
 		let mut okay: bool = false;
-			let mut new_user = UserCred {
-			name: String::new(),
-			access: 2,
-			salt: [0u8; 16],
-			pash: String::new(),
+		let mut new_user: UserCred = UserCred{
+			name: Vec::with_capacity(LARGE_BLOCK_SIZE),
+			access: 0,
+			salt: Vec::with_capacity(SMALL_BLOCK_SIZE),
+			pash: Vec::with_capacity(LARGE_BLOCK_SIZE),
 		};
-	
+
 		// Get a name to use; validate for no whitespace
 		println!("Enter a user name.  Names are case-sensitive, and may not contain whitespace.");
 		while !okay {
@@ -45,15 +92,40 @@ impl UserCred {
 			
 			println!("You entered \"{}\".", &tmp1);
 			for c in tmp1.chars() {
-				if c.is_ascii_whitespace() || tmp1.len() > MAX_ENTRY_LENGTH { okay = false; }
+				if c.is_ascii_whitespace() || tmp1.len() > LARGE_BLOCK_SIZE { okay = false; }
 			}
 			if !okay { 
 				println!("User names may not contain spaces, tabs, or newlines.");
 				tmp1.clear();
 			}
 		}
-		new_user.name = tmp1.as_bytes();
+		for c in tmp1.as_bytes() {
+			new_user.name.push(*c);
+		}
+		while new_user.name.len() < new_user.name.capacity() {
+			new_user.name.push(9u8);
+		}
 		okay = false;
+		tmp1.clear();
+
+		// Set read/write access
+		let mut n: u8 = 0;
+		okay = false;
+		while !okay {
+			println!("Set user access level:");
+			println!("    1) Read-Only");
+			println!("    2) Read & Edit");
+			print!(" > ");
+			out.flush()?;
+			inpt.read_line(&mut tmp1)?;
+			tmp1 = String::from(tmp1.trim());
+			n = tmp1.parse().expect("Valid access levels are 1 and 2.");
+			if (n < 1) || (n > 2) { okay = false; }
+			if !okay {
+				println!("Valid access levels are 1 and 2.");
+			}
+		}
+		new_user.access = n;
 		tmp1.clear();
 		
 		// Get a password to use; validate by double-checking
@@ -70,7 +142,7 @@ impl UserCred {
 			out.flush()?;
 			inpt.read_line(&mut tmp2)?;
 			tmp2 = String::from(tmp2.trim());
-			if tmp1 != tmp2 || tmp1.len() > MAX_ENTRY_LENGTH { okay = false; }
+			if tmp1 != tmp2 || tmp1.len() > LARGE_BLOCK_SIZE { okay = false; }
 			if !okay {
 				println!("Your first and second entries did not match.  Try again.");
 				tmp1.clear();
@@ -79,104 +151,55 @@ impl UserCred {
 		}
 		println!("Please remember your password, as it cannot be recovered.");
 		
-		// Set read/write access
-		okay = false;
-		while !okay {
-			println!("Set user access level:");
-			println!("    1) Read-Only");
-			println!("    2) Read & Edit");
-			print!(" > ");
-			out.flush()?;
-			inpt.read_line(&mut tmp1)?;
-			tmp1 = String::from(tmp1.trim());
-			let num: u8 = tmp1.parse().expect("Valid access levels are 1 and 2.");
-			if (num < 1) || (num > 2) { okay = false; }
-			if !okay {
-				println!("Valid access levels are 1 and 2.");
-			}
-		}
-		
 		// Get a salt value;  flushing out the first value produced by OsRng
 		// is recommended when using it early in the boot sequence; obviously
 		// that isn't the case here, but it should have been, so I'm including it.
-		let mut peanuts = [0u8; 16];
+		let mut peanuts = [0u8; SMALL_BLOCK_SIZE];
 		OsRng.fill_bytes(&mut peanuts);
-		peanuts = [0u8; 16];
+		peanuts = [0u8; SMALL_BLOCK_SIZE];
 		OsRng.fill_bytes(&mut peanuts);
 		
-		new_user.salt = peanuts;
+		//new_user.salt = peanuts;
+		for b in &peanuts {
+			new_user.salt.push(*b);
+		}
+		while new_user.salt.len() < new_user.salt.capacity() {
+			new_user.salt.push(9u8);
+		}
 		
 		// Hash the salted password string using the Argon2 algorithm
 		let hash = argon2::hash_encoded(tmp1.as_bytes(), &peanuts, &config)?;
-		new_user.pash = hash.split_at(28).1;
-		
+		for b in hash.split_at(28).1.as_bytes() { // 28 is the beginning of the actual hash value
+			new_user.pash.push(*b);
+		}
+		while new_user.pash.len() < new_user.pash.capacity() {
+			new_user.pash.push(9u8);
+		}
 		Ok(new_user)
 	}
-	
-	pub fn add_user(usr: UserCred) -> Result<bool, Box<dyn Error>> {
-		// TODO: Add a user to the key file
-		// Open key file
-		let key = File::open(KEY_FILE)?;
-		let mut reader = BufReader::new(&key);
-		let mut writer = BufWriter::new(&key);
-		let mut contents = String::new();
-		
-		let mut to_add: Vec<u8> = Vec::new();
-		for b in usr.name.as_bytes().iter() {
-			to_add.push(*b);
+
+	fn equals(&self, other: &UserCred) -> bool {
+		if self.name == other.name &&
+		   self.access == other.access &&
+		   self.salt == other.salt && 
+		   self.pash == other.pash {
+			true
+		} else {
+			false
 		}
-		','.encode_utf8(&mut to_add);
-		to_add.push(usr.access);
-		','.encode_utf8(&mut to_add);
-		for b in usr.salt.iter() {
-			to_add.push(*b);
-		}
-		','.encode_utf8(&mut to_add);
-		for b in usr.pash.as_bytes().iter() {
-			to_add.push(*b);
-		}
-		
-		writer.write(&to_add)?;
-		writer.flush()?;
-		
-		// Read key file into a vector; may not need this for "add"
-		reader.read_to_string(&mut contents)?;
-		let extant_creds: Vec<&str> = contents.split('\n').collect();
-		
-		Ok(true)
 	}
 	
-	pub fn verify(name: String, password: String) -> Result<bool, Box<dyn Error>> {
-		// TODO CHECKIT
-		// Open input stream
-		// let config = argon2::Config::default();
-		let key = File::open(KEY_FILE)?;
-		let mut reader = BufReader::new(key);
-		let mut contents = String::new();
-		
-		// Read key file into a vector
-		reader.read_to_string(&mut contents)?;
-		let extant_creds: Vec<&str> = contents.split('\n').collect();
-		
-		// DEBUG SCAFFOLDING; REMOVE FOR PRODUCTION
-		println!("There are currently {} sets of user credentials", extant_creds.len());
-		for usr in extant_creds {
-			println!("{}", usr);
-		}
-		
-		for cred in &extant_creds {
-			let fields: Vec<String> = vec!(cred.split(',').collect()?);
-			if name = fields.get(0)? {
-				if argon2::verify_encoded(&password, &fields.get(2)?.as_bytes()).unwrap() {
-					if fields.get(1)? = 1 {
-						Ok(false);
-					} else {
-						Ok(true);
-					}
-				}
+	fn verify(&self, key: Vec<UserCred>) -> Result<bool, &str> {
+		let mut verf: bool = false;
+		for other in key {
+			if self.equals(&other) {
+				verf = true;
 			}
 		}
-		Err(String::from("Unable to verify username/password combination."));
-
+		match verf {
+			true => Ok(self.access >= 2),
+			false => Err("Unable to verify user credentials.")
+		}
 	}
+		
 }
