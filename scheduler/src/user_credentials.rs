bb3ef8c66d01789e9bb8dd996fe09e73b2ea1f14
Nate Roberts <robernat@msudenver.edu>
2020-12-07 19:34:35 -0700

    Fixed a bug in credential parsing; login seems to be working now\n\nBut now all of the text fields are being rpassword'ed and I don't know why.

--- scheduler/src/user_credentials.rs
+++ scheduler/src/user_credentials.rs
@@ -3,8 +3,8 @@ extern crate rpassword;
 use std::boxed::Box;
 use std::error::Error;
 use std::fmt;
-use std::fs::{File, OpenOptions, ReadDir};
-use std::io::{self, BufReader, BufWriter, Read, Write};
+use std::fs::{File, OpenOptions};
+use std::io::{self, Read, Write};
 use std::path::Path;
 use rand::RngCore;
 use rand::rngs::OsRng;
@@ -14,7 +14,6 @@ const KEY_FILE: &str = "./.nothing.key";
 const LARGE_BLOCK_SIZE: usize = 128;
 const SMALL_BLOCK_SIZE: usize=16;
 const JUNK_BYTE: u8 = 33u8;
-const JUNK_RANGE: std::ops::Range<usize> = (169..256);
 
 pub struct UserCred {
 	name: Vec<u8>,
@@ -68,91 +67,7 @@ pub fn turn_key() -> Result<CredKey, &'static str> {
 	keylib.load()?;
 	keylib.parse()?;
 	Ok(keylib)
-	// let mut key = OpenOptions::new()
-	// 				.read(true)
-	// 				.write(true)
-	// 				.open(path)
-	// 				.expect("Unable to read key file.");
-	
-	// let mut contents: Vec<u8> = Vec::new();
-	// let mut key = File::open(path).or(
-	// 	OpenOptions::new().read(true).create(true).open(path))
-	// 				.expect("Could neither find nor create a key file.");
-	// key.read_to_end(&mut contents).expect("Unexpected error reading key.");
-	// let keylib = parse_key(contents);
-	// Ok(keylib)
 }
-
-// fn parse_key(inpt: Vec<u8>) -> Vec<UserCred> {
-// 	if inpt.len() < 145 {
-// 		return Vec::new()
-// 	}
-// 	let mut users: Vec<UserCred> = vec!();
-// 	let mut i: usize = 0;
-// 	while i < inpt.len() {
-// 		// TODO: Find a way to not use magic numbers
-// 		let user: UserCred = UserCred {
-// 			name: inpt[0..64].to_vec(),
-// 			access: inpt[64],
-// 			salt: inpt[65..81].to_vec(),
-// 			pash: inpt[81..145].to_vec(),
-// 		};
-// 		users.push(user);
-// 		i += 145;
-// 	}
-// 	users
-// }
-
-// // TODO: Seems unintuitive that adding a user currently requires 2 actions (push to key, update key); refactor?
-// pub fn update_key(update: Vec<UserCred>) -> Result<(), &'static str> {
-// 	let mut key = OpenOptions::new()
-// 				.read(true)
-// 				.append(true)
-// 				// .create_new(true)
-// 				.open(KEY_FILE)
-// 				.expect("Unable to open key file.");
-// 	//File::open(KEY_FILE).expect("Unable to open key file!");
-// 	//let mut reader = BufReader::new(key);
-// 	// let mut writer = BufWriter::new(key);
-// 	let mut data: Vec<u8> = Vec::new();
-// 	for mut user in update {
-// 		 println!("I am writing {}", user);
-// 		data.append(&mut user.name);
-// 		data.push(user.access.clone());
-// 		data.append(&mut user.salt);
-// 		data.append(&mut user.pash);
-// 	}
-// 	//println!("{}", String::from_utf8(data).expect("..."));
-// 	// writer.flush().expect("Unexpected failure writing to file.");
-// 	key.write_all(&data[..]).expect("Unexpected failure writing key file.");
-// 	Ok(())
-// }
-
-// pub fn verify_user(key: Vec<UserCred>, name: &str, password: &str) -> Result<bool, &'static str> {
-// 	let mut verified: bool = false;
-// 	let mut writer: u8 = 0;
-// 	let name_bytes: Vec<u8> = name.as_bytes().to_vec();
-// 	let pass_bytes: Vec<u8> = password.as_bytes().to_vec();
-// 	println!("Attempting to verify user credentials ({}, {})...", String::from_utf8_lossy(&name_bytes), String::from_utf8_lossy(&pass_bytes));
-// 	for user in key {
-// 		if user.name == name_bytes {
-// 			// let mut new_pash: Vec<u8> = Vec::with_capacity(LARGE_BLOCK_SIZE);
-// 			let config = argon2::Config::default();
-// 			let hash = argon2::hash_encoded(&pass_bytes, &user.salt, &config).expect("Error creating password hash.");
-// 			let new_pash = pack_vector(hash.split_at(28).1.as_bytes().to_vec(), LARGE_BLOCK_SIZE);
-// 			if user.pash == new_pash {
-// 				verified = true;
-// 				writer = user.access;
-// 				println!("User credentials verified!");
-// 				break;
-// 			}
-// 		}
-// 	}
-// 	match verified {
-// 		true => Ok(writer == 2),
-// 		false => Err("Unable to verify user credentials."),
-// 	}
-// }
 
 fn pack_vector(val: Vec<u8>, cap: usize) -> Vec<u8> {
 	let mut ret: Vec<u8> = Vec::with_capacity(cap);
@@ -171,15 +86,20 @@ impl UserCred {
 		let inpt = io::stdin();
 		let mut out = io::stdout();
 		let config = argon2::Config::default();
+		let new_name: Vec<u8>;
+		let new_access: u8;
+		let new_salt: Vec<u8>;
+		let new_pash: Vec<u8>;
 		let mut tmp1 = String::new();
 		let mut tmp2 = String::new();
 		let mut okay: bool = false;
-		let mut new_user: UserCred = UserCred {
-			name: Vec::with_capacity(LARGE_BLOCK_SIZE),
-			access: 0,
-			salt: Vec::with_capacity(SMALL_BLOCK_SIZE),
-			pash: Vec::with_capacity(LARGE_BLOCK_SIZE),
-		};
+		let new_user: UserCred;
+		// let mut new_user: UserCred = UserCred {
+		// 	name: Vec::with_capacity(LARGE_BLOCK_SIZE),
+		// 	access: 0,
+		// 	salt: Vec::with_capacity(SMALL_BLOCK_SIZE),
+		// 	pash: Vec::with_capacity(LARGE_BLOCK_SIZE),
+		// };
 
 		// Get a name to use; validate for no whitespace
 		println!("Enter a user name.  Names are case-sensitive, and may not contain whitespace.");
@@ -199,12 +119,13 @@ impl UserCred {
 				tmp1.clear();
 			}
 		}
-		for c in tmp1.as_bytes() {
-			new_user.name.push(*c);
-		}
-		while new_user.name.len() < new_user.name.capacity() {
-			new_user.name.push(JUNK_BYTE);
-		}
+		new_name = pack_vector(tmp1.as_bytes().to_vec(), LARGE_BLOCK_SIZE);
+		// for c in tmp1.as_bytes() {
+		// 	new_user.name.push(*c);
+		// }
+		// while new_user.name.len() < new_user.name.capacity() {
+		// 	new_user.name.push(JUNK_BYTE);
+		// }
 		okay = false;
 		tmp1.clear();
 
@@ -227,7 +148,8 @@ impl UserCred {
 				tmp1.clear();
 			}
 		}
-		new_user.access = n;
+		// new_user.access = n;
+		new_access = n;
 		tmp1.clear();
 		
 		// Get a password to use; validate by double-checking
@@ -257,24 +179,32 @@ impl UserCred {
 		OsRng.fill_bytes(&mut peanuts);
 		
 		//new_user.salt = peanuts;
-		for b in &peanuts {
-			new_user.salt.push(*b);
-		}
-		while new_user.salt.len() < new_user.salt.capacity() {
-			new_user.salt.push(JUNK_BYTE);
-		}
+		// for b in &peanuts {
+		// 	new_user.salt.push(*b);
+		// }
+		// while new_user.salt.len() < new_user.salt.capacity() {
+		// 	new_user.salt.push(JUNK_BYTE);
+		// }
+		new_salt = peanuts.to_vec();
 		
 		// Hash the salted password string using the Argon2 algorithm
-		let hash = argon2::hash_encoded(tmp1.as_bytes(), &peanuts, &config)?;
+		// let hash = argon2::hash_encoded(tmp1.as_bytes(), &peanuts, &config)?;
+		let hash = argon2::hash_encoded(tmp1.as_bytes(), &new_salt, &config)?;
 		println!("UserCred::new() has calculated the following hash:\n{}", hash);
-		for b in hash.split_at(28).1.as_bytes() { // 28 is the beginning of the actual hash value
-			new_user.pash.push(*b);
-		}
-		while new_user.pash.len() < new_user.pash.capacity() {
-			new_user.pash.push(JUNK_BYTE);
-		}
-		// if new_user.verify() - Before saving, I want to make sure the user doesn't already exist.  Probably going to need to add key to new() parameters, or just fucking write a create method like you should have a week ago you dumb idiot.
-		println!("UserCred::new() is creating:\n{}", new_user);
+		// for b in hash.split_at(28).1.as_bytes() { // 28 is the beginning of the actual hash value
+		// 	new_user.pash.push(*b);
+		// }
+		// while new_user.pash.len() < new_user.pash.capacity() {
+		// 	new_user.pash.push(JUNK_BYTE);
+		// }
+		new_pash = pack_vector(hash.split_at(28).1.as_bytes().to_vec(), LARGE_BLOCK_SIZE);
+		// println!("UserCred::new() is creating:\n{}", new_user);
+		new_user = UserCred {
+			name: new_name,
+			access: new_access,
+			salt: new_salt,
+			pash: new_pash,
+		};
 		Ok(new_user)
 	}
 
@@ -357,13 +287,17 @@ impl CredKey {
 			while i < self.raw.len() {
 				// TODO: Find a way to not use magic numbers
 				let user: UserCred = UserCred {
-					name: self.raw[0..64].to_vec(),
-					access: self.raw[64],
-					salt: self.raw[65..81].to_vec(),
-					pash: self.raw[81..145].to_vec(),
+					// name: self.raw[0..64].to_vec(),
+					name: self.raw[0..LARGE_BLOCK_SIZE].to_vec(),
+					// access: self.raw[64],
+					access: self.raw[LARGE_BLOCK_SIZE],
+					// salt: self.raw[65..81].to_vec(),
+					salt: self.raw[LARGE_BLOCK_SIZE+1..LARGE_BLOCK_SIZE+1+SMALL_BLOCK_SIZE].to_vec(),
+					// pash: self.raw[81..145].to_vec(),
+					pash: self.raw[(LARGE_BLOCK_SIZE)+1+SMALL_BLOCK_SIZE..(2*LARGE_BLOCK_SIZE)+1+SMALL_BLOCK_SIZE].to_vec(),
 				};
 				self.users.push(user);
-				i += 145;
+				i += (3*LARGE_BLOCK_SIZE)+1;
 			}
 		}
 		if self.users.len() < 1 {
@@ -422,33 +356,9 @@ impl CredKey {
 			false => Err("Unexpected error adding user."),
 		}
 	}
-		// TODO: Seems unintuitive that adding a user currently requires 2 actions (push to key, update key); refactor?
-	// pub fn update_key(update: Vec<UserCred>) -> Result<(), &'static str> {
-	// 	let mut key = OpenOptions::new()
-	// 				.read(true)
-	// 				.append(true)
-	// 				// .create_new(true)
-	// 				.open(KEY_FILE)
-	// 				.expect("Unable to open key file.");
-	// 	//File::open(KEY_FILE).expect("Unable to open key file!");
-	// 	//let mut reader = BufReader::new(key);
-	// 	// let mut writer = BufWriter::new(key);
-	// 	let mut data: Vec<u8> = Vec::new();
-	// 	for mut user in update {
-	// 		 println!("I am writing {}", user);
-	// 		data.append(&mut user.name);
-	// 		data.push(user.access.clone());
-	// 		data.append(&mut user.salt);
-	// 		data.append(&mut user.pash);
-	// 	}
-	// 	//println!("{}", String::from_utf8(data).expect("..."));
-	// 	// writer.flush().expect("Unexpected failure writing to file.");
-	// 	key.write_all(&data[..]).expect("Unexpected failure writing key file.");
-	// 	Ok(())
-	// }
 	
 	pub fn verify(&self, name: &str, password: &str) -> Option<&UserCred> {
-		let mut verified: &UserCred;
+		let verified: &UserCred;
 		let name_bytes = pack_vector(name.as_bytes().to_vec(), LARGE_BLOCK_SIZE);
 		let pass_bytes = password.as_bytes();
 		println!("Attempting to verify user credentials ({}, {})...", String::from_utf8_lossy(&name_bytes), String::from_utf8_lossy(&pass_bytes));
@@ -463,17 +373,6 @@ impl CredKey {
 					println!("Yeah, that's a user, dingus.");
 					return Some(verified)
 				}
-				// let mut new_pash: Vec<u8> = Vec::with_capacity(LARGE_BLOCK_SIZE);
-				// let config = argon2::Config::default();
-				// let hash = argon2::hash_encoded(&pass_bytes, &u.salt, &config).expect("Error creating password hash.");
-				// println!(" > > > This new hash is {}", String::from_utf8_lossy(&hash.split_at(28).1.as_bytes().to_vec()));
-				// let new_pash = pack_vector(hash.split_at(28).1.as_bytes().to_vec(), LARGE_BLOCK_SIZE);
-				// println!("Comparing hashes:\n{}\n{}", String::from_utf8_lossy(&new_pash), String::from_utf8_lossy(&u.pash));
-				// if u.pash == new_pash {
-				// 	verified = u;
-				// 	println!("User credentials verified!");
-				// 	return Some(verified)
-				// }
 			} else {
 				println!("NO HE NOT A SAME FROM {}", String::from_utf8_lossy(&u.name));
 			}
